11d1043a629331c469cff516e286f4cf53e8d16f
kemkem <kemkem@oort.(none)>
2013-06-09 20:53:47 +0200

    added statusPlaylist script

---
+++ sh/statusPlaylist.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PID_PATH=/home/kemkem/aalarmv2/arduinoAlarm/sh
+
+if [ -f $PID_PATH/mpg123.pid ];then
+	PID=`/bin/cat $PID_PATH/mpg123.pid`
+	PROCESS_COUNT=`ps -ef | grep -v grep | grep mpg123 | grep $PID | wc -l`
+	if [ $PROCESS_COUNT > 0 ];then
+		echo "Music playlist is running"
+	else
+		echo "Music playlist is not running"
+	fi
+else
+	echo "Music playlist is not running"
+fi
+
+
