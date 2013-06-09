b00c4ba595a10b8c237e33c429ea2c67bb9a0778
kemkem <kemkem@oort.(none)>
2013-06-09 20:24:20 +0200

    updated playlist scripts

--- sh/startPlaylist.sh
+++ sh/startPlaylist.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-/usr/bin/mpg123 -@ /home/kemkem/Electro/list -Z
+PID_PATH=/home/kemkem/aalarmv2/arduinoAlarm/sh
+PLAYLIST_PATH="/home/kemkem/Electro/list"
+
+if [ -f $PID_PATH/mpg123.pid ];then
+	echo "pid present : mpg123 already running"
+	exit 0
+else
+	/usr/bin/mpg123 -@ $PLAYLIST_PATH -Z > /dev/null 2>&1 &
+	echo $! > $PID_PATH/mpg123.pid 
+fi
+
--- sh/stopPlaylist.sh
+++ sh/stopPlaylist.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-/usr/bin/killall mpg123
+PID_PATH=/home/kemkem/aalarmv2/arduinoAlarm/sh
+
+if [ -f $PID_PATH/mpg123.pid ];then
+	PID=`/bin/cat $PID_PATH/mpg123.pid`
+	/bin/kill -9 $PID
+	rm $PID_PATH/mpg123.pid
+else
+	echo "no pid found. mpg123 is not running."
+fi
+
