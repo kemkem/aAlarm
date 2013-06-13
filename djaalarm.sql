9d591076add2c15aa8bacfd6ec48360574e28a25
kemkem <kemkem@oort.(none)>
2013-06-13 22:34:22 +0200

    new sql dump

--- djaalarm.sql
+++ djaalarm.sql
@@ -15,17 +15,15 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 --
 -- Dumping data for table `aalarm_command`
 --
 
 LOCK TABLES `aalarm_command` WRITE;
 /*!40000 ALTER TABLE `aalarm_command` DISABLE KEYS */;
-INSERT INTO `aalarm_command` VALUES (1,'Set Online','Set alarm to online state','setOnline'),(2,'Set Offline','Set alarm to offline state','setOffline');
+INSERT INTO `aalarm_command` VALUES (1,'Set Online','Set alarm to online state','setOnline'),(2,'Set Offline','Set alarm to offline state','setOffline'),(3,'start ZoneMinder','startZoneMinder','startZoneMinder'),(4,'stop ZoneMinder','stopZoneMinder','stopZoneMinder'),(5,'start Music Playlist','startMusicPlaylist','startMusicPlaylist'),(6,'stop Music Playlist','stopMusicPlaylist','stopMusicPlaylist');
 /*!40000 ALTER TABLE `aalarm_command` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 --
 -- Dumping data for table `aalarm_refsensortype`
@@ -33,10 +31,9 @@ UNLOCK TABLES;
 
 LOCK TABLES `aalarm_refsensortype` WRITE;
 /*!40000 ALTER TABLE `aalarm_refsensortype` DISABLE KEYS */;
-INSERT INTO `aalarm_refsensortype` VALUES (1,'Global'),(2,'Door Sensor');
+INSERT INTO `aalarm_refsensortype` VALUES (1,'Global'),(2,'Door Sensor'),(3,'Service');
 /*!40000 ALTER TABLE `aalarm_refsensortype` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 --
 -- Dumping data for table `aalarm_refstate`
@@ -44,10 +41,9 @@ UNLOCK TABLES;
 
 LOCK TABLES `aalarm_refstate` WRITE;
 /*!40000 ALTER TABLE `aalarm_refstate` DISABLE KEYS */;
-INSERT INTO `aalarm_refstate` VALUES (1,1,'offline','Offline','colorYellow'),(2,1,'timed','Online Timed','colorLightGreen'),(3,1,'online','Online','colorGreen'),(4,1,'intrusion','Intrusion','colorOrange'),(5,1,'warning','Warning','colorOrangeRed'),(6,1,'alert','Alert','colorRed'),(7,2,'open','Open','colorRed'),(8,2,'closed','Closed','colorGreen');
+INSERT INTO `aalarm_refstate` VALUES (1,1,'offline','Offline','colorYellow'),(2,1,'timed','Online Timed','colorLightGreen'),(3,1,'online','Online','colorGreen'),(4,1,'intrusion','Intrusion','colorOrange'),(5,1,'warning','Warning','colorOrangeRed'),(6,1,'alert','Alert','colorRed'),(7,2,'open','Open','colorRed'),(8,2,'closed','Closed','colorGreen'),(9,3,'Running','Running','colorGreen'),(10,3,'Stopped','Stopped','colorRed');
 /*!40000 ALTER TABLE `aalarm_refstate` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 --
 -- Dumping data for table `aalarm_sensor`
@@ -55,8 +51,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `aalarm_sensor` WRITE;
 /*!40000 ALTER TABLE `aalarm_sensor` DISABLE KEYS */;
-INSERT INTO `aalarm_sensor` VALUES (1,1,'Global','Global',0),(2,2,'Door1','Main Door',1);
+INSERT INTO `aalarm_sensor` VALUES (1,1,'Global','Global',0),(2,2,'Door1','Main Door',1),(3,3,'ZoneMinder','ZoneMinder',0),(4,3,'MusicPlaylist','Music Playlist',0);
 /*!40000 ALTER TABLE `aalarm_sensor` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
