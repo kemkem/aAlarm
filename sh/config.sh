de1e84f9423c26c1d20d93bfc2f31d0617a56ac9
kemkem <marc@kprod.net>
2013-06-25 17:31:21 +0200

    removed config files

--- sh/config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PATH_FOLDER_PID="/home/kemkem/Work/arduinoAlarm/sh"
-PATH_PLAYLIST="/home/kemkem/Electro/list"
-PATH_ZM_SOURCE="/usr/share/zoneminder/events/Door"
-PATH_ZM_TARGET="/home/kemkem/Work/djangoAAlarm/static/img/zmlast"
-
