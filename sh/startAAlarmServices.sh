2fd526a0d2840199b1be99722016184fa1960158
kemkem <marc@kprod.net>
2013-06-26 21:29:25 +0200

    mod start/stop service

--- sh/startAAlarmServices.sh
+++ sh/startAAlarmServices.sh
@@ -3,5 +3,8 @@
 . $AALARM_PATH_FOLDER_CONFIG/config.sh
 
 $PATH_CONTROLLER &
+echo $! > $PATH_FOLDER_PID/controller.pid
+
 $PATH_DJANGO_MANAGER runserver 0.0.0.0:8000 &
+echo $! > $PATH_FOLDER_PID/django.pid
 
--- sh/stopAAlarmServices.sh
+++ sh/stopAAlarmServices.sh
@@ -2,8 +2,21 @@
 
 . $AALARM_PATH_FOLDER_CONFIG/config.sh
 
-killall alarmSerialController.pl
-killall manage.py
 $PATH_FOLDER_SCRIPTS/stopPlaylist.sh
 $PATH_FOLDER_SCRIPTS/stopZM.sh
 
+if [ -f $PATH_FOLDER_PID/controller.pid ];then
+	PID=`/bin/cat $PATH_FOLDER_PID/controller.pid`
+	/bin/kill -9 $PID
+	rm $PATH_FOLDER_PID/controller.pid
+else
+	echo "no pid found. controller is not running."
+fi
+
+if [ -f $PATH_FOLDER_PID/django.pid ];then
+	PID=`/bin/cat $PATH_FOLDER_PID/django.pid`
+	/bin/kill -9 $PID
+	rm $PATH_FOLDER_PID/django.pid
+else
+	echo "no pid found. django is not running."
+fi
