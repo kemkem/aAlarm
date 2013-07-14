6ecfdd91c1d6150b98da72dd28ccf2b1dcd442e1
kemkem <marc@kprod.net>
2013-07-14 20:18:17 +0200

    fix zmlast bug : crash, cannot find list file (directory was not created)

--- sh/zmLast.sh
+++ sh/zmLast.sh
@@ -2,6 +2,8 @@
 
 . $AALARM_PATH_FOLDER_CONFIG/config.sh
 
+#TODO maybe init this directory in some install script
+mkdir -p $PATH_ZM_TARGET
 rm $PATH_ZM_TARGET/*
 find $PATH_ZM_SOURCE/ -cmin -5 -exec cp {} $PATH_ZM_TARGET \;
 find $PATH_ZM_TARGET -name '*analyse*jpg' -exec rm {} \;
