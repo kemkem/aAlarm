f2cf5417b4ab4ee263a37b2a6a303c50dced218a
marc <marc@tc2>
2014-11-24 01:14:29 +0100

    debian build fix path again

--- debian/build_debian.sh
+++ debian/build_debian.sh
@@ -3,7 +3,7 @@
 mkdir -p aalarm_package/usr/share/aAlarm
 mkdir -p aalarm_package/usr/bin
 
-cp -R ../sh aalarm_package/usr/bin
+cp ../sh/* aalarm_package/usr/bin
 cp -R ../conf aalarm_package/usr/share/aAlarm
 cp -R ../pl aalarm_package/usr/share/aAlarm
 cp -R ../webDj aalarm_package/usr/share/aAlarm
