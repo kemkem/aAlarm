7b8b63fd090e0038c01aa80aa8b4b13d4d0521b6
marc <marc@tc2>
2014-11-29 19:12:22 +0100

    deb : more path mod related to /etc

--- debian/build_debian.sh
+++ debian/build_debian.sh
@@ -5,7 +5,7 @@ mkdir -p aalarm_package/usr/bin
 mkdir -p aalarm_package/etc/aalarm
 
 cp ../sh/* aalarm_package/usr/bin
-cp -R ../conf aalarm_package/etc/aalarm
+cp -R ../conf/* aalarm_package/etc/aalarm
 cp -R ../pl aalarm_package/usr/share/aalarm
 cp -R ../webDj aalarm_package/usr/share/aalarm
 
