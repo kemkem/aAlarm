47335ba130b35f33131994fc3858ff1e60f013f7
marc <marc@tc2>
2014-11-30 14:44:39 +0100

    deb : fix paths

--- debian/build_debian.sh
+++ debian/build_debian.sh
@@ -5,9 +5,9 @@ mkdir -p aalarm_package/usr/share/aalarm
 mkdir -p aalarm_package/usr/share/aalarm/sh
 mkdir -p aalarm_package/etc/aalarm
 
-cp ../sh/* aalarm_package/usr/share/sh
-mv aalarm_package/usr/share/sh/startAAlarmServices.sh aalarm_package/usr/bin
-mv aalarm_package/usr/share/sh/stopAAlarmServices.sh aalarm_package/usr/bin
+cp ../sh/* aalarm_package/usr/share/aalarm/sh
+mv aalarm_package/usr/share/aalarm/sh/startAAlarmServices.sh aalarm_package/usr/bin
+mv aalarm_package/usr/share/aalarm/sh/stopAAlarmServices.sh aalarm_package/usr/bin
 cp -R ../conf/* aalarm_package/etc/aalarm
 cp -R ../pl aalarm_package/usr/share/aalarm
 cp -R ../webDj aalarm_package/usr/share/aalarm
